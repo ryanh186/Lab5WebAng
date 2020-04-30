bf6975024a6bbbf9be66933ea975ffbddb476ba8
lpcbusiness777 <64090030+lpcbusiness777@users.noreply.github.com>
2020-04-30 00:02:05 -0500

    added the stuff for creating the db

--- src/main/java/edu/ben/cmsc3330/Cmsc3330Application.java
+++ src/main/java/edu/ben/cmsc3330/Cmsc3330Application.java
@@ -1,13 +1,16 @@
 package edu.ben.cmsc3330;
 
-import org.springframework.boot.SpringApplication;
+import edu.ben.cmsc3330.data.basicfuntions.Inserts;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cmsc3330Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cmsc3330Application.class, args);
+		//SpringApplication.run(Cmsc3330Application.class, args);
+
+		Inserts i = new Inserts();
+		i.insertDestination();
 	}
 
 }
